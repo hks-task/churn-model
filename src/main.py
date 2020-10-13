3a2b020544dc04e5d6e5cdcd7552f88683a7253f
kaan.simsek <kaan.simsek@getir.com>
2020-10-13 21:14:54 +0300

    try-except added to main.py

--- src/main.py
+++ src/main.py
@@ -3,12 +3,16 @@ from utils import reduce_mem_usage
 from functions import transform_data, feature_engineering
 from model import run_lgb
 import constants
+import traceback
 
 
 if __name__ == "__main__":
-
-    df = read_data()
-    df = reduce_mem_usage(df, True)
-    df = transform_data(df)
-    df = feature_engineering(df, constants.BREAK_POINT)
-    predicted = run_lgb(df)
+    try:
+        df = read_data()
+        df = reduce_mem_usage(df, True)
+        df = transform_data(df)
+        df = feature_engineering(df, constants.BREAK_POINT)
+        run_lgb(df)
+    except Exception:
+        err = traceback.format_exc()
+        print(err)
